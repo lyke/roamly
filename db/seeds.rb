e6ccee20be61b5c0fcfb358916a3f68f0b0472d1
kevin ly <kev3010@outlook.fr>
2023-06-26 14:09:36 +0200

    configuration de devise, cloudinary, active storage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+User.destroy_all
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+kevin_picture = URI.open("https://res.cloudinary.com/dwl5s1v4k/image/upload/v1687640247/IMG_1373_2_erzt1v.jpg")
+user1 = User.new(
+  email: "kevin@example.fr",
+  password: "123456",
+  nickname: "kev",
+  first_name: "kevin",
+  last_name: "ly"
+)
+user1.photo.attach(io: kevin_picture, filename: "Kevin profil picture", content_type: "image/jpg")
+user1.save!
