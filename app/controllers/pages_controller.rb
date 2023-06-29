4a6f8129fce648cd76c93d5c93fc9920b06c523a
kevin ly <kev3010@outlook.fr>
2023-06-29 17:58:22 +0200

    views dashboard, past dashboard, my secret spot

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,31 @@
 class PagesController < ApplicationController
+  before_action :set_travels, only: %i[ dashboard past_dashboard ]
   def home
   end
 
   def dashboard
+  end
+
+  def past_dashboard
+  end
+
+  def secret_spot
+    @my_secrets = Place.where(params[:user] == current_user)
+  end
+
+  private
+
+  def set_travels
+    @travels = Travel.all
+    @future_travels = []
+    @past_travels = []
+    # .flatten
+    @travels.each do |travel|
+      if travel.ending_date >= Date.today
+        @future_travels << travel
+      else
+        @past_travels << travel
+      end
+    end
   end
 end
