0730a726c94b1990cf9459f6e48e47a05231e41e
kevin ly <kev3010@outlook.fr>
2023-07-06 16:54:49 +0200

    bug fix heure

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -2,8 +2,8 @@ class TravelsController < ApplicationController
   before_action :set_travel, only: %i[ show edit update destroy map ]
 
   def show
-    @beginning = @travel.start_hour
-    @ending = @travel.end_hour
+    # @beginning = @travel.start_hour
+    # @ending = @travel.end_hour
   end
 
   def new
